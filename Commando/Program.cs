102f139ba4c4a301f50e7b95c5b75f3b6d6dc78b
benjamin <2000beni770@gmail.com>
2025-05-27 09:44:53 +0300

    creat commando class

--- Commando/Program.cs
+++ Commando/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Commando
+{
+    private string Name;
+    private int Code;
+    private string[] Tools;
+    private string status;
 
-Console.WriteLine("Hello, World!");
+    public Commando(string name, int code)
+    {
+        this.Name = name;
+        this.Code = code;
+        this.Tools = new []{"Hammer", "chisel", "rope", "bag", "canteen"};
+    }
+
+    public void Walk()
+    {
+        this.status = "walking";
+        Console.WriteLine("the soldier walking");
+    }
+    public void Hide()
+    {
+        this.status = "hiding";
+        Console.WriteLine("the soldier hiding");
+    } 
+    public void Attack()
+    {
+        this.status = "attacking";
+        Console.WriteLine($"the soldier {this.Code} walking");
+    }
+    
+}
